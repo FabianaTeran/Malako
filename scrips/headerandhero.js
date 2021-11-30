d601e021ce1c36dfb3661830b1033dcf195dc6c6
Fabiana Teran <teranfabiana13@gmail.com>
2021-11-30 15:20:12 -0300

    Algunas animaciones

---
+++ scrips/headerandhero.js
@@ -0,0 +1,13 @@
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("abajo",window.scrollY > 0);
+})
+
+
+// hero-content
+var typed = new Typed('.auto-input', {
+    strings: ['Radiante', 'Sana' , 'luminosa'],
+    typeSpeed: 100,
+    backSpeed:100,
+    loop:true
+})
